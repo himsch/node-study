7f31283c1924ea26390cca6cf54739cafb2ebf02
song <55rsearchco@gmail.com>
2023-04-05 15:20:08 +0900

    11.5 응답에 대해서

--- express/app.js
+++ express/app.js
@@ -6,7 +6,10 @@ app.get("/sky/:id", (req, res, next) => {
   // console.log(req.headers);
   console.log(req.params);
   console.log(req.query);
-  res.send("hi!");
+
+  // res.json({ name: "song" });
+  // res.sendStatus(400);
+  res.setHeader("key", "value").status(201).send("created");
 });
 
 app.listen(8080);
